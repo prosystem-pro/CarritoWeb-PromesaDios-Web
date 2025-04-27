969899624df0d02d288e1d2208a957a291701a73
Victor Samines <saminesvictor@gmail.com>
2025-04-26 22:38:37 -0600

    Se agrego Nombre empresa en Entorno

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -1,4 +1,5 @@
 export const Entorno = {
   Produccion: true,
-  ApiUrl: 'https://carritoweb-promesadios-api.onrender.com/api/'
+  ApiUrl: 'https://carritoweb-promesadios-api.onrender.com/api/',
+  NombreEmpresa: 'PanaderiaPromesaDeDios'
 };
