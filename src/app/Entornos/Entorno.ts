4ab42b0710ff0626186552cae8ebbbaa1daeed0b
prosystem-pro <prosystem490@gmail.com>
2025-05-17 19:16:03 -0600

    Se cambió al backend de prueba

--- src/app/Entornos/Entorno.ts
+++ src/app/Entornos/Entorno.ts
@@ -1,8 +1,8 @@
 export const Entorno = {
     Produccion: false,
     // ApiUrl: 'http://localhost:1433/api/' 
-    // ApiUrl: 'https://carritoweb-promesadios-api.onrender.com/api/',
-    ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
+    ApiUrl: 'https://carritoweb-promesadios-api.onrender.com/api/',
+    // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
     NombreEmpresa: 'PanaderiaPromesaDeDios'
   };
   
