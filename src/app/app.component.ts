005215a2a81e32191f41d0d298653fb4fff82365
prosystem-pro <prosystem490@gmail.com>
2025-05-25 02:46:46 -0600

    Prueba 2 de tiempo pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,35 +55,17 @@ export class AppComponent implements OnInit {
     this.RegistrarTiempoPagina(tiempoFormato);
   }
 
-  // RegistrarTiempoPagina(tiempoFormateado: string): void {
-  //   const Datos = {
-  //     TiempoPromedio: tiempoFormateado,
-  //     Navegador: this.ObtenerNavegador()
-  //   };
+  RegistrarTiempoPagina(tiempoFormateado: string): void {
+    const Datos = {
+      TiempoPromedio: tiempoFormateado,
+      Navegador: this.ObtenerNavegador()
+    };
 
-  //   this.ReporteTiempoPaginaServicio.Crear(Datos).subscribe({
-  //     next: (Respuesta) => console.log(' Tiempo registrado con éxito:', Respuesta),
-  //     error: (Error) => console.error(' Error al registrar tiempo en página:', Error)
-  //   });
-  // }
-
-RegistrarTiempoPagina(tiempoFormateado: string): void {
-  const Datos = {
-    TiempoPromedio: tiempoFormateado,
-    Navegador: this.ObtenerNavegador()
-  };
-
-  if (Entorno.Produccion) {
-    const url = `${Entorno.ApiUrl}reportetiempopagina/crear`;
-    const blob = new Blob([JSON.stringify(Datos)], { type: 'application/json' });
-    navigator.sendBeacon(url, blob);
-  } else {
     this.ReporteTiempoPaginaServicio.Crear(Datos).subscribe({
-      next: (r) => console.log('Local: Tiempo registrado', r),
-      error: (e) => console.error(' Error en local', e)
+      next: (Respuesta) => console.log(' Tiempo registrado con éxito:', Respuesta),
+      error: (Error) => console.error(' Error al registrar tiempo en página:', Error)
     });
   }
-}
 
 
   formatearTiempo(ms: number): string {
