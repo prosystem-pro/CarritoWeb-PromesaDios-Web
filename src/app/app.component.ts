5553a09b0dda229886ff3e73fddda9c393282327
Victor Samines <saminesvictor@gmail.com>
2025-05-30 00:00:42 -0600

    Se quito sidebar redes sociales en contacto y otro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -121,6 +121,14 @@ export class AppComponent implements OnInit {
     return this.router.url.startsWith('/productos');
   }
 
+  esContacto(): boolean {
+    return this.router.url === '/contacto';
+  }
+
+  esOtro(): boolean {
+    return this.router.url === '/otro';
+  }
+
   esReporteProducto(): boolean {
     return this.router.url === '/reporte-producto';
   }
