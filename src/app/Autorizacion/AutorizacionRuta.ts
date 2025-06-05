f3eb49610f780440065548a8a579721663480474
prosystem-pro <prosystem490@gmail.com>
2025-06-05 10:39:10 -0600

    Rastrando error de ruta reportes

--- src/app/Autorizacion/AutorizacionRuta.ts
+++ src/app/Autorizacion/AutorizacionRuta.ts
@@ -9,14 +9,23 @@ export class AutorizacionRuta implements CanActivate {
 
   constructor(private LoginServicio: LoginServicio, private router: Router) {}
 
-  canActivate(
+ canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    if (this.LoginServicio.ValidarToken()) {
+    console.log('--- AutorizacionRuta: canActivate ---');
+    console.log('Ruta solicitada:', state.url);
+
+    const tokenValido = this.LoginServicio.ValidarToken();
+    console.log('Resultado de LoginServicio.ValidarToken():', tokenValido);
+
+    if (tokenValido) {
+      console.log('Token válido. Acceso permitido.');
       return true;
     } else {
+      console.warn('Token inválido. Redirigiendo a /login...');
       this.LoginServicio.EliminarToken();
+      console.log('Token eliminado. Redireccionando a login.');
       this.router.navigate(['/login']);
       return false;
     }
