42f0fd0bb42661a2952719c10ff6b9b217d09d68
prosystem-pro <prosystem490@gmail.com>
2025-06-15 00:20:44 -0600

    finalizado pruebas de ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -46,7 +46,7 @@ export const routes: Routes = [
   { path: 'otro', component: OtroComponent},
 
   //Rutas protegidas
-  { path: 'reporte-producto', component: ReporteProductoComponent },
+  { path: 'reporte-producto', component: ReporteProductoComponent,canActivate: [AutorizacionRuta] },
   { path: 'header-reporte', component: HeaderReporteComponent,canActivate: [AutorizacionRuta] },
   { path: 'reporte-vista', component: ReporteVistaComponent,canActivate: [AutorizacionRuta] },
   { path: 'reporte-red-social', component: ReporteRedSocialComponent,canActivate: [AutorizacionRuta] },
